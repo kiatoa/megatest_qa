9bf217e6484f97c1091391bcb0664d58db0b4490
matt <matt>
2016-09-11 01:08:02 +0000

    Added hint on how to compile to .so for user tabs

--- test-view.scm
+++ test-view.scm
@@ -1,9 +1,13 @@
+;; make an so like this:
+;;
+;; csc test-view.scm -s -o test-view.so
+
 (use iup)
 (import (prefix iup iup:))
 
-(define (test-view-gen tabs tab-num view-name view-cfgdat mtconfig-dat)
+(define (test-view-gen commondat tabs tab-num view-name view-cfgdat mtconfig-dat)
  (iup:vbox
    (iup:button "Pushme")))
 
-(define (test-view-updater tabs tab-num view-name view-cfgdat mtconfig-dat)
+(define (test-view-updater commondat tabs tab-num view-name view-cfgdat mtconfig-dat)
    #f)
