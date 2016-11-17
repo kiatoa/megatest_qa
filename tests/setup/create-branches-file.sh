0b8456871a52ac8279c11f1caad12679a672ddf5
matt <matt>
2016-11-17 06:20:33 +0000

    First steps implementation of cont-integ

--- tests/setup/create-branches-file.sh
+++ tests/setup/create-branches-file.sh
@@ -3,9 +3,11 @@
 # We receive the megatest fossil location via $MEGATEST_FOSSIL_FILE
 # Create candidates
 
-# first create list of open branches
+rm -f branches.txt skip_branches.txt all-branches-nodes.txt
+
+# first create list of open branches, ignore anything with "trunk" in the name.
 #
-fossil branch list -a -R $MEGATEST_FOSSIL_FILE | tr '*' ' '; |awk '{print $1}' > all-branches.txt
+fossil branch list -a -R $MEGATEST_FOSSIL_FILE | tr '*' ' ' |awk '{print $1}'|grep -v trunk > all-branches.txt
 
 # get the wiki list of nodes
 #
@@ -13,12 +15,14 @@ fossil wiki export tested_nodes -R $MEGATEST_FOSSIL_FILE > tested_nodes
 
 # for each branch get the last node
 for branch in $(cat all-branches.txt);do
-    node=$(fossil timeline -t ci  -W 0 v1.62|egrep -v ^=== | tr '[]' '  '| awk '{print $2}'|head -1)
-    if grep $node tested_nodes;then
-	echo $branch >> branches.txt
-	echo $node >> tested_nodes
+    node=$(fossil timeline -t ci  -W 0 "$branch" -R $MEGATEST_FOSSIL_FILE |egrep -v ^=== | tr '[]' '  '| awk '{print $2}'|head -1)
+    echo "$branch $node" >> all-branches-nodes.txt
+    if grep $node tested_nodes > /dev/null;then
+	echo "$branch $node" >> skip_branches.txt
+    else
+	echo "$branch" >> branches.txt
+	echo "$node $branch" >> tested_nodes
     fi
 done
 
-
-
+# fossil wiki commit tested_nodes tested_nodes -R $MEGATEST_FOSSIL_FILE 
