a239d7e753da6eb1fc3228eae621ad1564ab2b7e
bb <bb>
2017-04-02 09:32:52 +0000

    reproduced env pollution with cache

--- tests/env-pollution/epra/tests/test1/main.sh
+++ tests/env-pollution/epra/tests/test1/main.sh
@@ -3,21 +3,26 @@
 # Run your step here
 echo Got here!
 
-
 #start_step step1
 megatest -step step1 :state start :status n/a
-
-#logpro $MT_TEST_RUN_DIR/run_${MT_TEST_NAME}.logpro $MT_TEST_RUN_DIR/run_${MT_TEST_NAME}.html < $MT_TEST_RUN_DIR/run_${MT_TEST_NAME}.log >& /dev/null
 
 # mutate env var which is eval'ed in runconfigs.config
 echo "before PATH=$PATH"
 export PATH=/pollution:$PATH
+
 # force cache cleaning
 echo "after PATH=$PATH"
-rm $MT_RUN_AREA_HOME/lt/$MT_TARGET/$MT_RUNNAME/.megatest.cfg*
+
+
+touch kickoff # trigger test clean/restart
+sleep 10
 sleep $SLEEPYTIME
+#mv -v $MT_RUN_AREA_HOME/lt/$MT_TARGET/$MT_RUNNAME/.megatest.cfg* $MT_TEST_RUN_DIR/.
 megatest -step step1 :state end :status 0
+
+
 # force cache rebuild
 megatest -test-status :state COMPLETED :status PASS
+
 exit 0
 
